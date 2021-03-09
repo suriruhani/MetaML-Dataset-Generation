ee9fd3e89976764de9e362efeaccf949200cc8f1
suriruhani <suriruhani@gmail.com>
2021-03-09 11:57:14 +0800

    implement N1a

--- Experiment 1/main.py
+++ Experiment 1/main.py
@@ -188,7 +188,8 @@ def main(path, sep, is_last, policy_file, acc_file, acc_inc_file, acc_dec_file, 
                 y_train = list(zip(*train))[1]
 
                 # model = tree.DecisionTreeClassifier()
-                model = KNeighborsClassifier(n_neighbors=1)
+                # model = KNeighborsClassifier(n_neighbors=1)
+                model = GaussianNB()
                 model = model.fit(X_train, y_train)
                 prediction = model.predict(X_test)
 
@@ -210,7 +211,7 @@ def main(path, sep, is_last, policy_file, acc_file, acc_inc_file, acc_dec_file, 
 
                 if round == 0:
                     for i, id in enumerate(id_test):
-                        pred = helper_prediction[i]
+                        pred = prediction[i]
                         # pred = prediction[i] # store prediction for this run
                         # check for score by matching y label to prediction
                         score = 1 if (pred == dataset[int(id)][1]) else 0
@@ -229,7 +230,7 @@ def main(path, sep, is_last, policy_file, acc_file, acc_inc_file, acc_dec_file, 
                         #     policy_1a(dataset, int(id), num_attr+2, (1+wrong_zero/zero))
                         # for id in wrong_one_id:
                         #     dataset[int(id)][num_attr+2] *= (1+wrong_one/one)
-                        if score == 1:
+                        if score == 0:
                             policy_1a(dataset, int(id), num_attr+2, 2)
 
                 accuracy_value = accuracy_score(y_test, prediction)*100
